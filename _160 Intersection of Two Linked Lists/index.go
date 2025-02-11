7106559fd262deb6bcd27db8f071205ac85a6454
Pham Van Khang <phamvankhang.tvi@gmail.com>
2025-02-11 18:55:13 +0700

    _160

--- _160 Intersection of Two Linked Lists/index.go
+++ _160 Intersection of Two Linked Lists/index.go
@@ -1,1 +1,19 @@
-package _60_Intersection_of_Two_Linked_Lists
+package _160_Intersection_of_Two_Linked_Lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+
+	if headA == nil || headB == nil {
+		return nil
+	}
+	//dumb,
+	return headB
+}
+
+func Run(headA, headB *ListNode) *ListNode {
+	return getIntersectionNode(headA, headB)
+}
